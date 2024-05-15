1534449e40ef9b4df2e1653a153710f5a5f19608
Octavio Reinoso <oct.reinoso@gmail.com>
2024-05-14 23:20:39 -0300

    sec commit

--- simulacro1/Program.cs
+++ simulacro1/Program.cs
@@ -11,13 +11,13 @@ namespace simulacro1
         static void Main(string[] args)
         {
 
-            int rendimiento;
-            float sueldo;
-            float sueldoPasado;
+            //int rendimiento;
+            //float sueldo;
+            //float sueldoPasado;
 
             Console.WriteLine("Ingrese el nivel de rendimiento según su mes y posterior su sueldo: (1. Bueno 2. Regular 3. Malo):");
-            rendimiento = int.Parse(Console.ReadLine());
-            sueldo = int.Parse(Console.ReadLine());
+            float rendimiento = int.Parse(Console.ReadLine());
+            float sueldo = int.Parse(Console.ReadLine());
 
             if(rendimiento == 1) 
             {
@@ -33,7 +33,7 @@ namespace simulacro1
 
             Console.WriteLine("Sueldo a cobrar: $" + sueldo);
             Console.WriteLine("Ingrese su sueldo del mes pasado: ");
-            sueldoPasado = float.Parse(Console.ReadLine());
+            float sueldoPasado = float.Parse(Console.ReadLine());
             
             if(sueldo > sueldoPasado)
             {
@@ -43,6 +43,24 @@ namespace simulacro1
             {
                 Console.WriteLine("Su sueldo no aumento con respecto al del mes anterior. A seguir!");
             }
+
+            //calcular el costo de un viaje en taxi, considerando la tarifa base y los kilómetros recorridos.
+            //Deberás ingresar por teclado la distancia en kilómetros y el tiempo en minutos que duró el viaje.
+            //La tarifa base es de 50 pesos y se cobra un adicional de 10 pesos por cada kilómetro recorrido.
+            //Si el tiempo del viaje supera los 30 minutos, se agrega un cargo extra de 20 pesos.
+            //Calcula y muestra el costo total del viaje, incluyendo todos los cargos adicionales
+
+            Console.WriteLine("Ingrese los km recorridos y tiempo en min que duró el viaje: ");
+            int km = int.Parse(Console.ReadLine());
+            int minutos = int.Parse(Console.ReadLine());
+
+            int tarifa = 50 + (10 * km);
+            if(minutos > 30)
+            {
+                tarifa += 20; 
+            }
+
+            Console.WriteLine("Costo del viaje: $" + tarifa);
         }
     }
 }
